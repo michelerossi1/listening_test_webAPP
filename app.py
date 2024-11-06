0da162bd36c1169eef6b3aeee65963efc8b52ff6
Michele Rossi <mike00guitar@gmail.com>
2024-11-06 08:43:12 +0100

    another fake modification to test git

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = "secret_key"  # Required for session management
-random_modification = 'random_modification'
+random_modification2 = 'random_modification2'
 
 AUDIO_FOLDER = "static/music"
 USER_EVALUATIONS_FOLDER = "user_evaluations"  # Folder to store individual user evaluation files
